a62a0c4ae5e2e7eeec0c5c9056e45f798c4a9b6c
AbuhaithemAlthry <abuhaithemalthry@gmail.com>
2024-05-15 19:56:35 +0300

    final

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,33 +6,15 @@ Contains the number_of_subscribers function
 import requests
 
 
-BASE_URL = 'https://www.reddit.com'
-'''Reddit's base API URL.
-'''
-api_headers = {
-    'Accept': 'application/json',
-    'User-Agent': ' '.join([
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-        'AppleWebKit/537.36 (KHTML, like Gecko)',
-        'Chrome/97.0.4692.71',
-        'Safari/537.36',
-        'Edg/97.0.1072.62'
-    ])
-}
-
-
 def number_of_subscribers(subreddit):
 
     """returns the number of subscribers for a given subreddit"""
-    
-    if subreddit is None or type(subreddit) is not str:
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-
-    r = requests.get(
-        '{}/r/{}/about.json'.format(BASE_URL, subreddit),
-        headers=api_headers,
-        allow_redirects=False
-    )
-    if r.status_code == 200:
-        return r.json()['data']['subscribers']
-    return 0
