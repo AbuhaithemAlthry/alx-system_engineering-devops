d814eec3e43e89c1eaa015efed84b639844d9001
Seid <abuhaithemalthry@gmail.com>
2024-05-15 19:34:21 +0300

    fix (0-subs.py)

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,36 @@ Contains the number_of_subscribers function
 
 import requests
 
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.
+'''
+#!/usr/bin/python3
+'''A module containing functions for working with the Reddit API.
+'''
+import requests
+
+
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.
+'''
+api_headers = {
+    'Accept': 'application/json',
+    'User-Agent': ' '.join([
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+        'AppleWebKit/537.36 (KHTML, like Gecko)',
+        'Chrome/97.0.4692.71',
+        'Safari/537.36',
+        'Edg/97.0.1072.62'
+    ])
+}
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
-        return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+    r = requests.get(
+        '{}/r/{}/about.json'.format(BASE_URL, subreddit),
+        headers=api_headers,
+        allow_redirects=False
+    )
+    if r.status_code == 200:
+        return r.json()['data']['subscribers']
+    return 0
